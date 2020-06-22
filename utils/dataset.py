952cbea3385269779cbc334e43d1f83a9106f98f
Mário Cardoso <mario.j.c.cardoso@hotmail.com>
2020-06-22 11:43:28 +0000

    update

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,11 +1,12 @@
 import torch
 from torch.utils.data import Dataset
+import numpy as np
 
 class NCDFDatasets():
-	def __init__(self, data, val_split, test_split):
-		self.train_data = NCDFDataset(data, test_split, val_split, False, False)
-		self.val_data = NCDFDataset(data, test_split, val_split, False, True)
-		self.test_data = NCDFDataset(data, test_split, val_split, True, False)
+	def __init__(self, data, val_split, test_split, cut_y=False,  data_type='Prediction'):
+		self.train_data = NCDFDataset(data, test_split, val_split, data_type, False, False, cut_y)
+		self.val_data = NCDFDataset(data, test_split, val_split, data_type, False, True, cut_y)
+		self.test_data = NCDFDataset(data, test_split, val_split, data_type, True, False, cut_y)
 
 	def get_train(self):
 		return self.train_data
@@ -15,8 +16,10 @@ class NCDFDatasets():
 		return self.test_data
 
 class NCDFDataset(Dataset):
-	def __init__(self, data, test_split, val_split, is_test=False, is_val=False):
+	def __init__(self, data, test_split, val_split, data_type, is_test=False, is_val=False, cut_y=False):
 		super(NCDFDataset, self).__init__()
+		self.cut_y = cut_y
+		self.reconstruction = True if data_type == 'Reconstruction' else False 
 
 		splitter = DataSplitter(data, test_split, val_split)
 		if (is_test):
@@ -28,14 +31,35 @@ class NCDFDataset(Dataset):
 
 		#batch, channel, time, lat, lon
 		self.x = torch.from_numpy(dataset.x.values).float().permute(0, 4, 1, 2, 3)
-		self.y = torch.from_numpy(dataset.y.values).float().permute(0, 4, 1, 2, 3)#[:,:,0,:,:]
+		if (self.cut_y):
+			self.y = torch.from_numpy(dataset.y.values).float().permute(0, 4, 1, 2, 3)[:,:,0,:,:]
+		else:
+			self.y = torch.from_numpy(dataset.y.values).float().permute(0, 4, 1, 2, 3)
 		del dataset
 
+		if (self.reconstruction):
+			data_cat = torch.cat((self.x, self.y), 2)
+			self.y = data_cat.clone().detach()
+			self.x, self.removed = self.removeObservations(data_cat.clone().detach())
+
 	def __getitem__(self, index):
-		return (self.x[index,:,:5,:,:], self.y[index,:,:,:,:]#self.y[index,:,:,:])
+		if (self.reconstruction):
+			return (self.x[index,:,:,:,:], self.y[index,:,:,:,:], self.removed[index])
+		elif (self.cut_y):
+			return (self.x[index,:,:5,:,:], self.y[index,:,:,:])
+		else:
+			return (self.x[index,:,:5,:,:], self.y[index,:,:,:,:])
 
 	def __len__(self):
 		return self.x.shape[0]
+
+	def removeObservations(self, data):
+		removed_observations = torch.zeros(data.shape[0], dtype=torch.long)
+		for i in range(data.shape[0]):
+			index = np.random.randint(0, data.shape[2])
+			data[i,:,index,:,:] *= 0
+			removed_observations[i] = index
+		return data, removed_observations
 
 
 
---
+++ utils/trainer.py
@@ -0,0 +1,162 @@
+import torch
+import sys
+from gridmask import GridMask
+
+class Trainer():
+	def __init__(self, model, train_data, val_data, criterion, optimizer, max_epochs, device, path, cut_output = False,
+	 recurrent_model=False, patience=5, grid_mask=None, is_reconstruction = False, lilw=False):
+		self.model = model
+		self.train_data = train_data
+		self.val_data = val_data
+		self.criterion = criterion
+		self.max_epochs = max_epochs
+		self.device = device
+		self.optimizer = optimizer
+		self.cut_output = cut_output
+		self.path = path
+		self.grid = None
+		self.lilw = lilw
+		self.is_reconstruction = is_reconstruction
+		self.recurrent_model = recurrent_model
+		self.earlyStop = EarlyStop(patience, self.path)
+		if (grid_mask is not None):
+			self.grid = GridMask(grid_mask['d1'], grid_mask['d2'], device, grid_mask['ratio'], grid_mask['max_prob'], grid_mask['max_epochs'])
+
+	def train_evaluate(self):
+		train_losses = []
+		val_losses = []
+		if (self.grid is not None):
+			#self.grid.set_prob(epoch)
+			print(self.grid.get_prob())
+		for epoch in range(self.max_epochs):
+			self.train(train_losses)
+			print('Train - Epoch %d, Epoch Loss: %f' % (epoch, train_losses[epoch]))
+			self.evaluate(val_losses)
+			print('Val Avg. Loss: %f' % (val_losses[epoch]))
+			if (torch.cuda.is_available()):
+				torch.cuda.empty_cache()
+			if (self.earlyStop.check_stop_condition(epoch, self.model, self.optimizer, val_losses[epoch])):
+				break
+		return train_losses, val_losses
+
+	def train(self, train_losses):
+		train_loss = self.model.train()
+		epoch_train_loss = 0.0
+		for i, (x, y, removed) in enumerate(self.train_data):
+			x,y,removed = x.to(self.device), y.to(self.device), removed.to(self.device)
+			if (self.grid is not None):
+				x_grid = self.grid(x)
+			self.optimizer.zero_grad()
+			x_in = x if self.grid == None else x_grid
+			if (self.recurrent_model and not self.is_reconstruction):
+				states = self.init_hidden(x.size()[0], x.size()[3]*x.size()[4])
+				if (self.lilw):
+					output = self.model(x_in,states, original_x = x)
+				else:
+					output = self.model(x_in,states)
+			else:
+				output = self.model(x_in)
+			#batch : channel : time-steps : lat : lon
+			if (self.cut_output and not self.recurrent_model):
+				loss = self.criterion(output[:,:,0,:,:], y[:,:,0,:,:])
+			else:
+				loss = self.criterion(output, y, removed)
+			loss.backward()
+			self.optimizer.step()
+			epoch_train_loss += loss.detach().item()
+		avg_epoch_loss = epoch_train_loss/len(self.train_data)
+		train_losses.append(avg_epoch_loss)
+
+	def evaluate(self, val_losses):
+		epoch_val_loss = 0.0
+		self.model.eval()
+		with torch.no_grad():
+			for i, (x, y, removed) in enumerate(self.val_data):
+				x,y, removed = x.to(self.device), y.to(self.device), removed.to(self.device)
+				if (self.recurrent_model and not self.is_reconstruction):
+					states = self.init_hidden(x.size()[0], x.size()[3]*x.size()[4])
+					output = self.model(x,states)
+				else:
+					output = self.model(x)
+				if (self.cut_output and not self.recurrent_model):
+					loss = self.criterion(output[:,:,0,:,:], y[:,:,0,:,:])
+				else:
+					loss = self.criterion(output, y, removed)
+				epoch_val_loss += loss.detach().item()
+		avg_val_loss = epoch_val_loss/len(self.val_data)
+		val_losses.append(avg_val_loss)
+
+	def load_model(self):
+		checkpoint = torch.load(self.path)
+		self.model.load_state_dict(checkpoint['model_state_dict'])
+		self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+		epoch = checkpoint['epoch']
+		loss = checkpoint['loss']
+		return self.model, self.optimizer, epoch, loss
+
+	def init_hidden(self, batch_size, hidden_size):
+		h = torch.zeros(batch_size,hidden_size, device=self.device)
+		return (h,h)
+
+
+
+class EarlyStop:
+	def __init__(self, threshold, path):
+		self.min_loss = sys.float_info.max
+		self.count = 0
+		self.threshold = threshold
+		self.path = path
+        
+	def check_stop_condition(self, epoch, model, optimizer, loss):
+		if (loss < self.min_loss):
+			self.save_model(epoch, model, optimizer, loss)
+			self.min_loss = loss
+			self.count = 0
+			return False
+		else:
+			self.count += 1
+			if (self.count >= self.threshold):
+				return True
+			return False
+
+	def save_model(self, epoch, model, optimizer, loss):
+		torch.save({
+			'epoch': epoch,
+			'model_state_dict': model.state_dict(),
+			'optimizer_state_dict': optimizer.state_dict(),
+			'loss': loss,
+			}, self.path)
+		print ('=> Saving a new best')
+
+class Tester():
+	def __init__(self, model, test_data, criterion, optimizer, device, cut_output, recurrent_model=False):
+		self.model = model
+		self.test_data = test_data
+		self.criterion = criterion
+		self.device = device
+		self.optimizer = optimizer
+		self.cut_output = cut_output
+		self.recurrent_model = recurrent_model
+
+	def test(self):
+		batch_test_loss = 0.0
+		self.model.eval()
+		with torch.no_grad():
+			for i, (x, y, removed) in enumerate(self.test_data):
+				x,y, removed = x.to(self.device), y.to(self.device), removed.to(self.device)
+				if (self.recurrent_model):
+					states = self.init_hidden(x.size()[0], x.size()[3]*x.size()[4])
+					output = self.model(x, states)
+				else:
+					output = self.model(x)
+				if (self.cut_output and not self.recurrent_model):
+					loss = self.criterion(output[:,:,0,:,:], y[:,:,0,:,:])
+				else:
+					loss = self.criterion(output, y, removed)
+				batch_test_loss += loss.detach().item()
+		test_loss = batch_test_loss/len(self.test_data)
+		return test_loss
+
+	def init_hidden(self, batch_size, hidden_size):
+		h = torch.zeros(batch_size,hidden_size, device=self.device)
+		return (h,h)
